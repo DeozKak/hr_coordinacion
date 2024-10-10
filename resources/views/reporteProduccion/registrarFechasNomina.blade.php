a8f18770f2e96d0a82c73e4d0f9652d5805ae945
Jorge <jagutierrez8108@misena.edu.co>
2024-10-10 13:54:08 -0500

    Update registrarFechasNomina.blade.php

--- resources/views/reporteProduccion/registrarFechasNomina.blade.php
+++ resources/views/reporteProduccion/registrarFechasNomina.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Fechas nomina')
 
 @section('content')
-<link rel="stylesheet" href="{{ asset('css/nomina/fechasParametro.css') }}">
+<link rel="stylesheet" href="{{ asset('css/reporteProduccion/fechasParametro.css') }}">
 
 <div class="container">
     <div class="row justify-content-center">
@@ -134,5 +134,5 @@
 @endsection
 
 @section('js')
-<script src="{{ asset('js/nomina/fechasParametro.js') }}"></script>
+<script src="{{ asset('js/reporteProduccion/fechasParametro.js') }}"></script>
 @endsection
