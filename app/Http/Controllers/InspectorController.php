760f703b0681d69da16bf1cd75be1ea43330b41b
Jorge <jagutierrez8108@misena.edu.co>
2024-11-13 14:23:31 -0500

    correccion funcion sin cerrar

--- app/Http/Controllers/InspectorController.php
+++ app/Http/Controllers/InspectorController.php
@@ -19,7 +19,7 @@ class InspectorController extends Controller
     {
         $supervisores = User::role('Supervisor')->get();
         return view('inspectores.index', compact('supervisores'));
-   
+    }
 
     public function store(){
         $validatedData = request()->validate([
