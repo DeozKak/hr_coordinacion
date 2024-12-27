8ec88975596d12064e128ba045d97d0ef8305444
Jorge <jagutierrez8108@misena.edu.co>
2024-12-27 12:00:53 -0500

    cambio de version css produccion

--- resources/views/produccion/detalles.blade.php
+++ resources/views/produccion/detalles.blade.php
@@ -11,7 +11,7 @@
 <div id="overlay"></div>
 
 <input type="hidden" id="fecha_inicio" value="{{session('fecha_inicio')}}">
-<link rel="stylesheet" href="{{asset('css/produccion/produccion.css')}}">
+<link rel="stylesheet" href="{{asset('css/produccion/produccionV2.css')}}">
 <script src="{{asset('js/producciondetallesV3-9.js')}}?v={{ time()}}"></script>
 
 
@@ -20,16 +20,11 @@
 <input type="hidden" name="_token" id="token" value="{{csrf_token()}}">
 
 <div class="shadow-container">
-
-
     <a class="btn btn-primary" href="javascript:history.go(-1)" style="margin-bottom: 10px;">Ir Atrás</a>
     <button type="button" class="btn btn-success" id="exportar" style="margin-bottom: 10px;">Exportar</button>
     <x-adminlte-card title="Producción por dia" theme="info" icon="fas fa-code-branch" header-class="text-uppercase rounded-bottom border-info" collapsible>
         <div id="detalles" style="width: '100px'"></div>
     </x-adminlte-card>
- 
-       
-   
 </div>
 
 <!-- Modal Detalles de dia -->
